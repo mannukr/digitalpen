01f5dc332e0987ef0cbf3e8726b852c69923e42b
Mannu Kumar <mannukumar7891@gmail.com>
2019-09-28 23:28:25 +0530

    files uploaded

---
+++ resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,111 @@
+<section class="contact py-5" id="contact">
+    <div class="container py-lg-3">
+        <h3 class="heading mb-5 text-center" data-aos="zoom-in"> Get in touch</h3>
+        <form action="#" method="post" data-aos="fade-up">
+            <div class="row">
+                <div class="col-md-6 styled-input mt-0">
+                    <input type="text" name="Name" placeholder="First Name" required="">
+                </div>
+                <div class="col-md-6 styled-input mt-md-0">
+                    <input type="text" name="Name" placeholder="Last Name" required="">
+                </div> 
+            </div>
+            <div class="row">
+                <div class="col-md-6 styled-input">
+                    <input type="email" name="Email" placeholder="Email" required=""> 
+                </div>
+                <div class="col-md-6 styled-input">
+                    <input type="text" name="phone" placeholder="Phone Number" required="">
+                </div> 
+            </div>
+            <div class="styled-input">
+                <textarea name="Message" placeholder="Message" required=""></textarea>
+            </div>
+            <div class="click text-center mt-3">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
+</section>
+<!-- //contact -->
+
+<!--footer -->
+<footer>
+<section class="footer footer_w3layouts_section_1its py-5">
+    <div class="container py-md-4">
+    <div class="footer-grid_section text-center" data-aos="zoom-in">
+                <div class="footer-title mb-3">
+                    <a href="#"><i class="fab fa-blackberry mr-2"></i> DigitalPen</a>
+                </div>
+                <div class="footer-text">
+                    <p>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem ipnut libero malesuada feugiat.
+                     Lorem ipsum dolor sit amet, consectetur elit.</p>
+                </div>
+                <ul class="social_section_1info">
+                    <li class="mb-2 facebook"><a href="#"><i class="fab mr-1 fa-facebook-f"></i>facebook</a></li>
+                    <li class="mb-2 twitter"><a href="#"><i class="fab mr-1 fa-twitter"></i>twitter</a></li>
+                    <li class="google"><a href="#"><i class="fab mr-1 fa-google-plus-g"></i>google</a></li>
+                    <li class="linkedin"><a href="#"><i class="fab mr-1 fa-linkedin-in"></i>linkedin</a></li>
+                </ul>
+            </div>
+            
+        <div class="row footer-top mt-md-5 mt-4">
+            <div class="col-lg-4 col-md-6 footer-grid_section_1its_w3" data-aos="fade-right">
+                <div class="footer-title">
+                    <h3>Address</h3>
+                </div>
+                <div class="footer-text">
+                    <p>Address : 1234 lock, Charlotte, North Carolina, United States</p>
+                    <p>Phone : +12 534894364</p>
+                    <p>Email : <a href="mailto:info@example.com">info@example.com</a></p>
+                    <p>Fax : +12 534894364</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-md-0 mt-4 footer-grid_section_1its_w3" data-aos="fade-up">
+                <div class="footer-title">
+                    <h3>Quick Links</h3>
+                </div>
+                <div class="row">
+                    <ul class="col-6 links">
+                        <li><a href="#choose" class="scroll">Why Choose Us </a></li>
+                        <li><a href="#overview" class="scroll">Overview </a></li>
+                        <li><a href="#pricing" class="scroll">Pricing Plans</a></li>
+                        <li><a href="#faq" class="scroll">Faq's </a></li>
+                        <li><a href="#testimonials" class="scroll">Testimonial </a></li>
+                        <li><a href="#contact" class="scroll">Contact </a></li>
+                    </ul>
+                    <ul class="col-6 links">
+                        <li><a href="#">Privacy Policy </a></li>
+                        <li><a href="#">General Terms </a></li>
+                        <li><a href="#faq" class="scroll">Faq's </a></li>
+                        <li><a href="#">Knowledge </a></li>
+                        <li><a href="#">Forum </a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12 mt-lg-0 mt-4 col-sm-12 footer-grid_section_1its_w3" data-aos="fade-left">
+                <div class="footer-title">
+                    <h3>Newsletter</h3>
+                </div>
+                <div class="footer-text">
+                    <p>By subscribing to our mailing list you will always get latest news and updates from us.</p>
+                    <form action="#" method="post">
+                        <input type="email" name="Email" placeholder="Enter your email..." required="">
+                        <button class="btn1"><i class="fas fa-paper-plane" aria-hidden="true"></i></button>
+                        <div class="clearfix"> </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</footer>
+<!-- //footer -->
+
+<!-- copyright -->
+<div class="cpy-right text-center py-3">
+    <p class="">© DigitalPen. All rights reserved | 
+       
+    </p>
+</div>
+<!-- //copyright -->
---
+++ resources/views/frontend/includes/nav-bar.blade.php
@@ -0,0 +1,48 @@
+<nav class="main-header">
+    <div id="brand" data-aos="zoom-in-up">
+      <div id="logo">
+        <a href="index.html">
+          <i class="fab fa-blackberry"></i>
+        </a>
+      </div>
+      <div id="word-mark">
+        <h1>
+          <a href="index.html">digitalpen.com</a>
+        </h1>
+      </div>
+    </div>
+    <div id="menu">
+      <div id="menu-toggle">
+        <div id="menu-icon">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+        </div>
+      </div>
+      <ul class="text-center text-capitalize nav-agile" data-aos="zoom-in-up">
+        <li>
+          <a href="index.html" class="active">home</a>
+        </li>
+        <li>
+          <a href="#choose" class="scroll">Why Choose Us</a>
+        </li>
+        <li>
+          <a href="#overview" class="scroll">Overview</a>
+        </li>
+        <li>
+          <a href="#pricing" class="scroll">Pricing</a>
+        </li>
+        <li>
+          <a href="#faq" class="scroll">Faq</a>
+        </li>
+        <li>
+          <a href="#contact" class="scroll">Contact</a>
+        </li>
+        <li>
+          <button type="button" class="btn w3ls-btn" data-toggle="modal" aria-pressed="false" data-target="#exampleModalCenter2">
+            Login
+          </button>
+        </li>
+      </ul>
+    </div>
+  </nav>
